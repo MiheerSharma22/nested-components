41ff072f6e28bff298b5ba897e8f1cefb663167d
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-08-12 14:08:12 +0530

    added styling and changed the layout somewhat

--- src/components/TagView.js
+++ src/components/TagView.js
@@ -25,14 +25,58 @@ const TagView = ({ tag, onAddChild, onEditName, onEditData }) => {
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={handleToggleCollapse}>{collapsed ? ">" : "v"}</button>
-        <input type="text" value={tag.name} onChange={onEditName} />
-        <input type="text" value={tag.data} onChange={onEditData} />
-        <button onClick={onAddChild}>Add Child</button>
+    <div className=" w-full mt-2 bg-gray-200 border-2 border-blue-400">
+      {/* header */}
+      <div className="px-1 py-1 bg-blue-400 flex items-center justify-between">
+        {/* left */}
+        <div className="flex ">
+          {/* collapse button */}
+          <button
+            onClick={handleToggleCollapse}
+            className=" mr-2 px-[1.3rem] py-[0.3rem] flex items-center bg-gray-100 "
+          >
+            {collapsed ? ">" : "v"}
+          </button>
+
+          {/* tag name */}
+          <input
+            type="text"
+            value={tag.name}
+            className="bg-transparent font-semibold"
+            onChange={(event) => onEditName(tag, event.target.value)}
+          />
+        </div>
+
+        {/* right */}
+        {/* add child button */}
+        <button
+          onClick={() => onAddChild(tag)}
+          className=" px-[1.3rem] py-[0.3rem] flex items-center bg-gray-100 "
+        >
+          Add Child
+        </button>
       </div>
-      {renderChildren(tag.children || [])}
+
+      {/* data / children container */}
+      {/* if not collapsed then show the window */}
+      {!collapsed && (
+        <div className="p-3 flex flex-col gap-2">
+          {/* if current tag has no children then show its data else render its children */}
+          {!tag.children ? (
+            <span className="flex items-center gap-2">
+              <p>Data</p>
+              <input
+                type="text"
+                className="border border-gray-500 rounded-sm px-[0.3rem] py-[0.3rem]"
+                value={tag.data}
+                onChange={(event) => onEditData(tag, event.target.value)}
+              />
+            </span>
+          ) : (
+            !collapsed && renderChildren(tag.children || [])
+          )}
+        </div>
+      )}
     </div>
   );
 };
